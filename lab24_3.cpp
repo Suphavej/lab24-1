61adaf211e56c9dee66399a0317b91eec2a136ed
Suphavej <fatkimmmm@gmail.com>
2026-03-09 21:38:59 +0700

    sed

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
-//Write function count() here.
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int count(int data[], int size){
+    set<int> uniqueNumbers(data, data + size);
+    return uniqueNumbers.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
